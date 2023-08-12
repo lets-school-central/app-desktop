6dc2061a559a4e2573777a2a14af1cf1e1ca35df
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-08-12 19:24:13 +0200

    feat: initialize database and create app_handle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
 mod handlers;
 mod utils;
 mod state;
+mod database;
+mod constants;
 
 use handlers::{
     gameinstallation::{
@@ -20,17 +22,9 @@ use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(debug_assertions)]
-            {
-                let window = app.get_window("main").unwrap();
-                window.open_devtools();
-                window.close_devtools();
-            }
-            Ok(())
-        })
-        .manage(state::State {
-            game_path: "".to_string().into(),
+        .manage(state::AppState {
+            game_path: Default::default(),
+            db: Default::default(),
         })
         .invoke_handler(tauri::generate_handler![
             check_game_installation,
@@ -39,6 +33,22 @@ fn main() {
             install_mod_loader,
             check_mod_loader
         ])
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+
+            let handle = app.handle();
+
+            let app_state: tauri::State<state::AppState> = handle.state();
+            let db = database::initialize_database(&handle).expect("error while initializing database");
+            *app_state.db.lock().unwrap() = Some(db);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
