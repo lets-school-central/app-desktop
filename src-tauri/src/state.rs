82621290ee0d6cbd38d47a9f2dd6cdb2e7dd4ae1
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-08-12 19:24:35 +0200

    feat: create service access

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -1,5 +1,38 @@
+use std::path::PathBuf;
+use rusqlite::Connection;
 use std::sync::Mutex;
+use tauri::{AppHandle, State, Manager};
 
-pub struct State {
-    pub game_path: Mutex<String>
+pub struct AppState {
+    pub game_path: Mutex<Option<String>>,
+    pub db: Mutex<Option<Connection>>,
+}
+
+pub trait ServiceAccess {
+    fn db<F, TResult>(&self, operation: F) -> TResult where F: FnOnce(&Connection) -> TResult;
+    fn get_game_path(&self) -> PathBuf;
+    fn set_game_path(&self, path: String);
+}
+
+impl ServiceAccess for AppHandle {
+    fn db<F, TResult>(&self, operation: F) -> TResult where F: FnOnce(&Connection) -> TResult {
+        let app_state: State<AppState> = self.state();
+        let db_connection_guard = app_state.db.lock().unwrap();
+        let db = db_connection_guard.as_ref().unwrap();
+
+        operation(db)
+    }
+
+    fn get_game_path(&self) -> PathBuf {
+        let app_state: State<AppState> = self.state();
+        let game_path_guard = app_state.game_path.lock().unwrap();
+        let game_path = game_path_guard.as_ref().unwrap();
+
+        PathBuf::from(game_path.as_str())
+    }
+
+    fn set_game_path(&self, path: String) {
+        let app_state: State<AppState> = self.state();
+        *app_state.game_path.lock().unwrap() = Some(path);
+    }
 }
