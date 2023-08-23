30e19d862e8a8d452098432893f51b80c3a846bc
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-08-23 12:24:10 +0200

    feat: better relation fields

--- src/models/mod.ts
+++ src/models/mod.ts
@@ -1,15 +1,18 @@
 import { Record } from "pocketbase";
 
+import type { User } from "./user";
+
 export declare class Mod extends Record {
 	id: string;
 	title: string;
 	description: string;
 	screenshots: string[];
+	author: User["id"];
 }
 
 export declare class ModVersion extends Record {
 	id: string;
-	mod: Mod;
+	mod: Mod["id"];
 	version: string;
 	changelog: string;
 	fileUrl: string;
